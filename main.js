49b918ec240bfee5292a95e4d916932ba226f630
ZeSpongeBob <n.piveteau@outlook.fr>
2022-06-01 17:56:08 +0200

    hot reloading & structure

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const electronReload = require('electron-reload')
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -10,8 +11,12 @@ const createWindow = () => {
     }
   })
 
-  win.loadFile('index.html')
+  win.loadFile('./src/page/index.html')
 }
+
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+});
 
 app.whenReady().then(() => {
   createWindow()
